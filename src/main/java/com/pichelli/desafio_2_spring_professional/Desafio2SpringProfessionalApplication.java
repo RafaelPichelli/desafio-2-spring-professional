f3bdb8b8f3329b599669101e37414c33607659f5
RafaelPichelli <rafael.pichelli4@hotmail.com>
2024-11-26 16:05:54 -0300

    Inicio do projeto

---
+++ src/main/java/com/pichelli/desafio_2_spring_professional/Desafio2SpringProfessionalApplication.java
@@ -0,0 +1,13 @@
+package com.pichelli.desafio_2_spring_professional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio2SpringProfessionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio2SpringProfessionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pichelli/desafio_2_spring_professional/Desafio2SpringProfessionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichelli.desafio_2_spring_professional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio2SpringProfessionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
